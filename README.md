17c8c4e942a79c02e0c215dcc8f080802c6b29e9
Dmitry Stropaloff <helions8@gmail.com>
2011-11-27 23:56:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+IvyAndroidExample
+================
+
+Sample code for the article, published on Habrahabr.ru. 
+Sample android application using Apache Ivy as a dependency manager.
+
---
+++ src/com/example/IvyAndroidExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IvyAndroidExampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
